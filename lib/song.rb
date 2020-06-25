b5e769ab9bfd72ba9c105a42e77ba08671c5f631
Lisa Feng <lisafeng10@gmail.com>
2020-06-25 19:10:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,5 +46,11 @@ class Song
     self.all.sort_by{|song| song.name}
   end
   
+  def self.new_from_filename(filename)
+    
+    song = self.new
+    song.name = song_name
+    song
+  end
   
 end
