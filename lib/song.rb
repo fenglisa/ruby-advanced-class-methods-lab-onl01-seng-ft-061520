5caf5ba3b4d1b7cb97f71aede4779b23c6370dbc
Lisa Feng <lisafeng10@gmail.com>
2020-06-25 18:42:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,9 +34,9 @@ class Song
   
   def self.find_or_create_by_name(song_name)
     if self.all.include?(song_name)
-      self.name
+      self.find_by_name(song_name)
     else
-      self.create_by_name
+      self.create_by_name(song_name)
     end
   end
   
