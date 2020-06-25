df55047efc25e795c098446230c80a8aff8bf141
Lisa Feng <lisafeng10@gmail.com>
2020-06-25 18:29:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,20 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    self.all << self.new
+    @@all.last
+  end
+  
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+  
+  def self.create_by_name(song_name)
+    @@all << self.new_by_name
+  end
+  
 end
